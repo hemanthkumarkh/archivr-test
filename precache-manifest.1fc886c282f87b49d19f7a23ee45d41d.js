7c4ac92108ad99f72867e8275af60f2fb4f455c6
khhemanthkumar <kh.hemanthkumar@gmail.com>
2022-06-26 14:28:07 +0530

    Updates

---
+++ precache-manifest.1fc886c282f87b49d19f7a23ee45d41d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "08438053a69c9107080f",
+    "url": "/archivr-test/static/css/main.22117ca9.chunk.css"
+  },
+  {
+    "revision": "08438053a69c9107080f",
+    "url": "/archivr-test/static/js/main.08438053.chunk.js"
+  },
+  {
+    "revision": "0898ee64aa9ce4c3bc3c",
+    "url": "/archivr-test/static/js/1.0898ee64.chunk.js"
+  },
+  {
+    "revision": "5ad553731091433ebe5e",
+    "url": "/archivr-test/static/js/runtime~main.5ad55373.js"
+  },
+  {
+    "revision": "81400773905a9ea7c2d4b1078d009f0e",
+    "url": "/archivr-test/index.html"
+  }
+];
